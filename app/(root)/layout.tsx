fb91b7519229fe1a2deaaf4a2c49a47346ef9ae3
dmitridrozdov <dm.drozdov@gmail.com>
2024-06-23 12:37:37 +1000

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-// import LeftSidebar from "@/components/LeftSidebar";
+import LeftSidebar from "@/components/LeftSidebar";
 // import MobileNav from "@/components/MobileNav";
 
 import Image from "next/image";
@@ -12,8 +12,7 @@ export default function RootLayout({
   return (
     <div className="relative flex flex-col">
       <main className="relative flex bg-black-3">
-        {/* <LeftSidebar /> */}
-        <p>LEFT SIDE BAR</p>
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-4 sm:px-14">
           <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
             <div className="flex h-16 items-center justify-between md:hidden">
