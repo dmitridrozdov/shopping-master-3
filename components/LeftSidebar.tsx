f03cd6aab508a9bb57bad03a93edab7dbadab1b3
dmitridrozdov <dm.drozdov@gmail.com>
2024-06-25 11:18:12 +1000

    Update LeftSidebar.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { cn } from '@/lib/utils'
+import Link from 'next/link'
 
 const LeftSidebar = () => {
   return (
     <section className={cn("left_sidebar h-[calc(100vh-5px)]")}>
-        <nav>
-            Left side bar
+        <nav className="flex flex-col gap-6">
+            <Link href='/'>List</Link>
+            <Link href='/add'>Add Product</Link>
         </nav>
     </section>
   )
