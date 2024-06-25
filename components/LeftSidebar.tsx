0f9df35ecfd27383305dcbdfa30af9fb7f805386
dmitridrozdov <dm.drozdov@gmail.com>
2024-06-25 11:14:32 +1000

    Update LeftSidebar.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { cn } from '@/lib/utils'
 
 const LeftSidebar = () => {
   return (
-    <section>
-        <nav></nav>
+    <section className={cn("left_sidebar h-[calc(100vh-5px)]")}>
+        <nav>
+            Left side bar
+        </nav>
     </section>
   )
 }
