3a960c7def74834df9ca7fbc4a5dadb3c17f8ccf
dmitridrozdov <dm.drozdov@gmail.com>
2024-06-26 08:35:48 +1000

    Update LeftSidebar.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -6,8 +6,12 @@ const LeftSidebar = () => {
   return (
     <section className={cn("left_sidebar h-[calc(100vh-5px)]")}>
         <nav className="flex flex-col gap-6">
-            <Link href='/'><h1 className="text-24 font-extrabold max-lg:hidden">List</h1></Link>
-            <Link href='/add'><h1 className="text-24 font-extrabold max-lg:hidden">Add Product</h1></Link>
+            <Link href='/' >
+                <h1 className="text-24 font-extrabold max-lg:hidden">List</h1>
+            </Link>
+            <Link href='/add' >
+                <h1 className="text-24 font-extrabold max-lg:hidden">Add Product</h1>
+            </Link>
         </nav>
     </section>
   )
