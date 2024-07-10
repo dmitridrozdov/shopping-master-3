bd8d7ec4838dac6faed0d26c39f3e375dcd0d8df
dmitridrozdov <dm.drozdov@gmail.com>
2024-07-10 15:47:24 +1000

    Create auth.config.ts

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://real-marmot-92.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
