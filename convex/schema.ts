3d49067851f76cc400d741e8dd891bf7bd44d63e
dmitridrozdov <dm.drozdov@gmail.com>
2024-07-10 15:47:29 +1000

    Update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,10 +2,27 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  products: defineTable({
+
+  currentproducts: defineTable({
+    user: v.id('users'),
+    authorId: v.string(),
+    authorImageUrl: v.string(),
     category: v.string(),
     product: v.string(),
   })
     .searchIndex('search_product', { searchField: 'product' })
-    .searchIndex('search_category', { searchField: 'category' })
+    .searchIndex('search_category', { searchField: 'category' }),
+
+    users: defineTable({
+        email: v.string(),
+        clerkId: v.string(),
+        name: v.string(),
+    }),
+
+    products: defineTable({
+        category: v.string(),
+        product: v.string(),
+      })
+        .searchIndex('search_product', { searchField: 'product' })
+        .searchIndex('search_category', { searchField: 'category' }),
 })
