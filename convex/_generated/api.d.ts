37a5daea69e3c74f3204b40ee2121c605d8831ee
dmitridrozdov <dm.drozdov@gmail.com>
2024-07-31 12:27:13 +1000

    Update api.d.ts

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,6 +15,7 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as http from "../http.js";
+import type * as list from "../list.js";
 import type * as products from "../products.js";
 import type * as tasks from "../tasks.js";
 import type * as users from "../users.js";
@@ -29,6 +30,7 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
+  list: typeof list;
   products: typeof products;
   tasks: typeof tasks;
   users: typeof users;
