f59278472d0b36368eaf42214df7b8c07f0712c4
dmitridrozdov <dm.drozdov@gmail.com>
2024-07-31 12:27:06 +1000

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,84 @@
-"use client";
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
+"use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { useMutation, useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { useToast } from "@/components/ui/use-toast"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  product: z.string().min(2, {
+    message: "Product must be at least 2 characters.",
+  }),
+})
 
 const Home = () => {
-  // const tasks = useQuery(api.tasks.get);
+
+  const createProduct = useMutation(api.list.createProduct)
+  const currentProducts = useQuery(api.list.get)
+
+  const { toast } = useToast()
+
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      product: "",
+    },
+  })
+  
+  // 2. Define a submit handler.
+  function onSubmit(product: z.infer<typeof formSchema>) {
+    createProduct({
+      product: product.product,
+      category: "test",
+    })
+      .then(() => {
+        toast({ title: product.product + ' created' });
+        form.reset(); // Reset the form fields
+      })
+      .catch((error) => {
+        console.error('Error creating product:', error);
+      });
+  }
+
   return (
-    <div className="mt-3 ml-3 flex flex-col">
-      <section>Add products</section>
-      <section className="flex items-center mt-2">
-        <Input />
-        <Button className="ml-3">Add</Button>
-      </section>
+    <div className="mt-2 ml-2 flex flex-col w-full">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex items-center">
+          <FormField
+            control={form.control}
+            name="product"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Product</FormLabel>
+                <FormControl>
+                  <Input placeholder="Product" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="ml-2">Add</Button>
+        </form>
+      </Form>
       <div>
-        {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
-      </div>
+        {currentProducts?.map(({ _id, product }) => <div key={_id}>{product}</div>)}
+      </div> 
     </div>
   )
 }
