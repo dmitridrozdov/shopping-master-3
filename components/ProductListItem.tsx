7d2f3a3ebdd847e2c52de77da790c4e53cf7f32e
dmitridrozdov <dm.drozdov@gmail.com>
2024-08-19 16:44:26 +1000

    Update ProductListItem.tsx

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -5,9 +5,10 @@ import { Id } from "@/convex/_generated/dataModel";
 interface Product {
   id: Id<"currentproducts">;
   product: string;
+  borderColor: string;
 }
 
-const ProductListItem = ({ id, product }: Product) => {
+const ProductListItem = ({ id, product, borderColor }: Product) => {
 
   const deleteProduct = useMutation(api.list.deleteProduct)
 
@@ -16,7 +17,7 @@ const ProductListItem = ({ id, product }: Product) => {
   };
 
   return (
-    <div key={id} className="cursor-pointer product-list-item bg-nav-focus border-l-8 border-rose-600" onClick={handleClick}>
+    <div key={id} className={`cursor-pointer product-list-item bg-nav-focus border-l-8 ${borderColor}`} onClick={handleClick}>
       <span className="list-font ml-2">{product}</span>
     </div>
   );
