89a576331d6b38e0bce69a7c6c5416c04d36c251
dmitridrozdov <dm.drozdov@gmail.com>
2024-08-21 17:13:06 +1000

    Update ProductListItem.tsx

--- components/ProductListItem.tsx
+++ components/ProductListItem.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { api } from "@/convex/_generated/api"
 import { useMutation } from "convex/react"
-import { Id } from "@/convex/_generated/dataModel";
+import { Id } from "@/convex/_generated/dataModel"
+import { useToast } from "@/components/ui/use-toast"
 
 interface Product {
   id: Id<"currentproducts">;
@@ -11,13 +13,27 @@ interface Product {
 const ProductListItem = ({ id, product, borderColor }: Product) => {
 
   const deleteProduct = useMutation(api.list.deleteProduct)
+  const [isDeleting, setIsDeleting] = useState(false)
+  const { toast } = useToast()
 
   const handleClick = () => {
-    deleteProduct({ id });
-  };
+    setIsDeleting(true);
+    deleteProduct({ id })
+      .then(() => {
+        toast({ title: product + ' removed' });
+      })
+      .catch((error) => {
+        // Handle deletion errors (e.g., show an error message)
+        setIsDeleting(false);
+      })
+  }
 
   return (
-    <div key={id} className={`cursor-pointer product-list-item bg-nav-focus border-l-8 ${borderColor}`} onClick={handleClick}>
+    <div
+      key={id}
+      className={`cursor-pointer product-list-item bg-nav-focus border-l-8 ${borderColor} ${isDeleting ? "fade-out" : ""}`}
+      onClick={handleClick}
+    >
       <span className="list-font ml-2">{product}</span>
     </div>
   );
