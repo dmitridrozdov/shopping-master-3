8caf0496c2614d1cb029f65579aa226d1b52bea5
dmitridrozdov <dm.drozdov@gmail.com>
2024-08-21 14:55:09 +1000

    Create ProductSearch.tsx

---
+++ components/ProductSearch.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useQuery } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import ProductSearchResult from './ProductSearchResult';
+
+interface ProductSearchProps {
+  inputValue: string;
+}
+
+const ProductSearch: React.FC<ProductSearchProps> = ({ inputValue }) => {
+
+  const selectedProducts = inputValue === '' ? [] : useQuery(api.products.getProductsBySearch, { search: inputValue })
+  const selectedFilteredProducts = 
+    selectedProducts?.filter(item => item.product.startsWith(inputValue.toLowerCase()));
+
+  return (
+    <div className="mt-3">
+      <p>
+          {selectedFilteredProducts?.map((product, index) => (
+            <ProductSearchResult key={index} productName={product.product} />
+          ))}
+        </p>
+    </div>
+  )
+}
+
+export default ProductSearch
